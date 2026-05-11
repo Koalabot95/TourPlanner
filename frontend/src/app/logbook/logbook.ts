eb70e0333d9fdbb4320e76f9ab521e129475e4c4
mfatihy70 <mfatihyildiz1453@gmail.com>
2026-05-11 10:56:46 +0200

    generated angular components, solved small error on tsconfig

---
+++ frontend/src/app/logbook/logbook.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logbook',
+  imports: [],
+  templateUrl: './logbook.html',
+  styleUrl: './logbook.scss',
+})
+export class Logbook {}
---
+++ frontend/src/app/tour-planner/tour-planner.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-planner',
+  imports: [],
+  templateUrl: './tour-planner.html',
+  styleUrl: './tour-planner.scss',
+})
+export class TourPlanner {}
