f533a84eeed79f4acfa6cf69a33e83ffc349573a
mfatihy70 <mfatih@ik.me>
2026-05-19 22:57:36 +0200

    moved components and pages out of view folder and deleted it

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { Home } from './views/pages/home/home';
-import { Login } from './views/pages/login/login';
-import { CreateTour } from './views/pages/create-tour/create-tour';
-import { CreateTourLog } from './views/pages/create-tour-log/create-tour-log';
-import { Profile } from './views/pages/profile/profile';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
+import { CreateTour } from './pages/create-tour/create-tour';
+import { CreateTourLog } from './pages/create-tour-log/create-tour-log';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
