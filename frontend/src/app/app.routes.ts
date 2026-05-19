9616fbd0ecbabe76bf200f18b1e7ed3676cf0a23
mfatihy70 <mfatih@ik.me>
2026-05-19 18:30:56 +0200

    created home page, which will contain tours and tour logs

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { Home } from './views/pages/home/home';
 import { Login } from './views/pages/login/login';
 import { TourPlanner } from './views/pages/tour-planner/tour-planner';
 import { Logbook } from './views/pages/logbook/logbook';
@@ -6,6 +7,7 @@ import { Profile } from './views/pages/profile/profile';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'tour-planner', component: TourPlanner },
   { path: 'logbook', component: Logbook },
