b8b23849da6a3360b59cf96efa6dbbd6c2cec9c3
mfatihy70 <mfatih@ik.me>
2026-05-19 18:10:19 +0200

    fixed sign out redirecting

--- frontend/src/app/views/pages/profile/profile.ts
+++ frontend/src/app/views/pages/profile/profile.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
-  imports: [Navbar],
+  imports: [Navbar, RouterLink],
   templateUrl: './profile.html',
   styleUrl: './profile.scss',
 })
