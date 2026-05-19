04d18003d70dedc8500548fcb61ac6a03147c239
mfatihy70 <mfatihyildiz1453@gmail.com>
2026-05-19 12:48:58 +0200

    deleted dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-
 import { Login } from './views/pages/login/login';
-import { Dashboard } from './views/pages/dashboard/dashboard';
 import { TourPlanner } from './views/pages/tour-planner/tour-planner';
 import { Logbook } from './views/pages/logbook/logbook';
 import { Profile } from './views/pages/profile/profile';
@@ -9,7 +7,6 @@ import { Profile } from './views/pages/profile/profile';
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'dashboard', component: Dashboard },
   { path: 'tour-planner', component: TourPlanner },
   { path: 'logbook', component: Logbook },
   { path: 'profile', component: Profile },
