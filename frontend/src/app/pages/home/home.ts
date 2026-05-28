2e6d714264b0d28d91033666038792333b5ba66f
mfatihy70 <mfatihyildiz1453@gmail.com>
2026-05-28 13:43:02 +0200

    updated home component to use the reusable card component. removed unneded style from global css

--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -3,32 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Navbar } from '../../components/navbar/navbar';
 import { Button } from '../../components/button/button';
+import { Tour } from '../../models/tour.model';
+import { TourLog } from '../../models/tour-log.model';
+import { Card } from '../../components/card/card';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, Navbar, RouterLink, Button],
+  imports: [CommonModule, Navbar, RouterLink, Button, Card],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
-
-/**
- * Home component serves as the landing page of the application. 
- * It displays a summary of tours and tour logs.
- * It reads data from local storage and binds it to the view for display. 
- * The component uses Angular's OnInit lifecycle hook to load data when the component is initialized.
- */
 export class Home implements OnInit {
-  // ViewModel State
-  tours: any[] = [];
-  tourLogs: any[] = [];
+  tours: Tour[] = [];
+  tourLogs: TourLog[] = [];
 
-  // Runs automatically when the component loads
   ngOnInit() {
     this.loadData();
   }
 
   loadData() {
-    // Read from local storage and assign to our ViewModel properties
     this.tours = JSON.parse(localStorage.getItem('tours') || '[]');
     this.tourLogs = JSON.parse(localStorage.getItem('tourLogs') || '[]');
   }
