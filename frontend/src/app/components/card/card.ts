6379bb365e1be930612d4bd8ddf8dbf4efa183b2
mfatihy70 <mfatihyildiz1453@gmail.com>
2026-05-28 13:40:52 +0200

    created card component

---
+++ frontend/src/app/components/card/card.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.html',
+  styleUrl: './card.scss',
+})
+export class Card {
+  @Input() title: string = '';
+  @Input() customClass: string = '';
+}
