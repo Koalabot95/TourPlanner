60893a6cd45e0f6efcf374a15a51d8dd32b50c16
daniel <daniel.feike@gmx.at>
2026-06-24 17:34:07 +0200

    added tour.service

---
+++ frontend/src/app/services/tour.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Tour } from '../Models/Tour';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TourService {
+  
+  private apiUrl = '/api/tours';
+
+  constructor(private http: HttpClient) { }
+
+  // Alle Touren vom Backend laden
+  getTours(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  // Eine neue Tour an das Backend senden und in der DB speichern
+  createTour(tour: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, tour);
+  }
+}
